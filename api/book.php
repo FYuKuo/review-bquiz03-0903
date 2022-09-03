424002a2f967187ef45a0ed876be7fd80099c1ed
FY Kuo <teresa99339@gmail.com>
2022-09-03 20:00:14 +0800

    訂票座位畫面完成 功能尚未完成

--- api/book.php
+++ api/book.php
@@ -1,4 +1,17 @@
-<h1>123</h1>
-<h1>123</h1>
-<h1>1fffff3</h1>
-<h1>123</h1>
+<div class="d-f book_set_bg m-auto f-w">
+    <div class="book_set d-f m-auto f-w">
+        <?php
+    for($i=0 ; $i<20 ; $i++) {
+    ?>
+        <div class="set_item d-f f-w">
+            <div class="ct w-100">
+                <?=floor($i/5)+1?>排<?=floor($i%5)+1?>號
+            </div>
+            <input type="checkbox" name="set" class="set" value="<?=$i?>" style="align-self: end;">
+        </div>
+        <?php
+    }
+    ?>
+
+    </div>
+</div>
